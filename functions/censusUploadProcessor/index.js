b55209e6ee77ead0a6aabe56c77f356c6220df93
Guy Harwood <guy@milkchip.com>
2018-05-17 16:42:25 +0100

    working function app

---
+++ functions/censusUploadProcessor/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const uuid = require('uuid/v4')
+
+module.exports = function (context, censusFile) {
+  context.log('JavaScript blob trigger function processed blob \n Name:',
+    context.bindingData.name, '\n Blob Size:', censusFile.length, 'Bytes')
+  context.bindings.outputTable = []
+
+  context.bindings.outputTable.push({
+    PartitionKey: 'TestBlob',
+    RowKey: uuid(),
+    Name: context.bindingData.name
+  })
+
+  context.done()
+}
